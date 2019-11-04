be990ee8aee5151bdd12e22a4270bce8723cb951
Cataline Baert <snuf_kin@yahoo.com>
2019-11-04 19:09:33 +0100

    add and import Flat component

--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Flat from './flat';
 
 const FlatList = () => {
   return (
     <div className="flat-list">
       <h1>Hello from FlatList component!</h1>
+      <Flat />
     </div>
   );
 };
