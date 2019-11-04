0b5b02147f7f946348387cc280f6de6f28780b23
Cataline Baert <snuf_kin@yahoo.com>
2019-11-04 19:13:47 +0100

    create new component

---
+++ src/components/marker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Marker = () => {
+  return (
+    <div className="marker">
+      <h1>Hello from Marker component!</h1>
+    </div>
+  );
+};
+
+export default Marker;
