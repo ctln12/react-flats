fc6f27738377dae264342b414c9facc1a6d391e2
Cataline Baert <snuf_kin@yahoo.com>
2019-11-04 20:55:05 +0100

    change link to map

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import flats from '../../data/flats';
 import FlatList from './flat_list';
-import Map from './map';
+import SimpleMap from './simple_map';
 
 const App = () => {
   return (
     <div className="app">
       <FlatList />
-      <Map />
+      <SimpleMap />
     </div>
   );
 };
