fba7760c12cf2bf608397d902a0109695450f1b8
Cataline Baert <snuf_kin@yahoo.com>
2019-11-04 19:46:21 +0100

    change content

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Flat = () => {
   return (
-    <div className="flat">
-      <h1>Hello from Flat component!</h1>
+    <div className="card">
+      <div className="card-category">150 EUR</div>
+      <div className="card-description">
+        <h2>Super 60m2 in trendy neighborhood!</h2>
+      </div>
+      <a className="card-link" href="#" />
     </div>
   );
 };
+
+// style='background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg;);'
 
 export default Flat;
