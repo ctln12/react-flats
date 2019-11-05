80742e4f33035fd1caa966e4e2f2ecbe8ad7e3d1
Cataline Baert <snuf_kin@yahoo.com>
2019-11-05 22:45:04 +0100

    refactor component

--- src/components/marker.jsx
+++ src/components/marker.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-const Marker = () => {
-  return (
-    <div className="marker">
-      <h1>Hello from Marker component!</h1>
-    </div>
-  );
-};
+const Marker = () => <div className="marker">&nbsp;</div>;
 
 export default Marker;
