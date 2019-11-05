94eeb83ec91e4576e2bb66dfbf5f43d313f50ee2
Cataline Baert <snuf_kin@yahoo.com>
2019-11-05 23:05:44 +0100

    modify display of price

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -7,12 +7,11 @@ class Flat extends Component {
   }
 
   render() {
-    const { flatInfo } = this.props;
+    const { flatInfo, selected } = this.props;
     return (
-      <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${flatInfo.imageUrl})` }}>
+      <div className={`card${selected ? ' active' : ''}`} style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${flatInfo.imageUrl})` }}>
         <div className="card-category">
-          {flatInfo.price}
-          {flatInfo.priceCurrency}
+          {`${flatInfo.price} ${flatInfo.priceCurrency}`}
         </div>
         <div className="card-description">
           <h2>{flatInfo.name}</h2>
