9f60de7d657598fb5cb2925aa804ac7b345621bf
Cataline Baert <snuf_kin@yahoo.com>
2019-11-05 22:40:13 +0100

    change function to class and add onClick function

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Flat = (props) => {
-  const { flatInfo } = props;
-  return (
-    <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${flatInfo.imageUrl})` }}>
-      <div className="card-category">
-        {flatInfo.price}
-        {flatInfo.priceCurrency}
+class Flat extends Component {
+  handleClick = () => {
+    const { selectFlat, index } = this.props;
+    selectFlat(index);
+  }
+
+  render() {
+    const { flatInfo } = this.props;
+    return (
+      <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${flatInfo.imageUrl})` }}>
+        <div className="card-category">
+          {flatInfo.price}
+          {flatInfo.priceCurrency}
+        </div>
+        <div className="card-description">
+          <h2>{flatInfo.name}</h2>
+        </div>
+        <a className="card-link" href="#" onClick={this.handleClick} />
       </div>
-      <div className="card-description">
-        <h2>{flatInfo.name}</h2>
-      </div>
-      <a className="card-link" href="#"></a>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Flat;
