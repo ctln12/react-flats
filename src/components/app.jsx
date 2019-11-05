d48a089179c9b0a4b4b45c18c1a5b7a8b7862c82
Cataline Baert <snuf_kin@yahoo.com>
2019-11-05 22:42:18 +0100

    change function to class, add constructor, set state, implement selectFlat function and add props to FlatList

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import flats from '../../data/flats';
 import FlatList from './flat_list';
-import SimpleMap from './simple_map';
 
-const App = () => {
-  return (
-    <div className="app">
-      <FlatList flats={flats} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      allFlats: flats
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  render() {
+    const { selectedFlat, allFlats } = this.state;
+    return (
+      <div className="app">
+        <FlatList
+          flats={allFlats}
+          selectedFlat={selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
