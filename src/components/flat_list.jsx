b075d6b690f9c0e87d581fd75355b00574a4a4e1
Cataline Baert <snuf_kin@yahoo.com>
2019-11-05 22:41:12 +0100

    add props

--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Flat from './flat';
 
 const FlatList = (props) => {
-  const { flats } = props;
+  const { flats, selectedFlat, selectFlat } = props;
   return (
     <div className="flat-list">
-      {flats.map(flat => <Flat flatInfo={flat} key={flat.id} />)}
+      {flats.map((flat, index) => {
+        return (
+          <Flat
+            flatInfo={flat}
+            key={flat.id}
+            selected={flat.name === selectedFlat.name}
+            index={index}
+            selectFlat={selectFlat}
+          />
+        );
+      })}
     </div>
   );
 };
